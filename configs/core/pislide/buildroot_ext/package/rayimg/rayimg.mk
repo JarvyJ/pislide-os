8721fd3d17b10adec41b290c9f2d1c7d32e127ef
Jarvy Jarvison <88567611+JarvyJ@users.noreply.github.com>
2025-08-31 09:13:02 -0500

    started trying to build

--- configs/core/pislide/buildroot_ext/package/rayimg/rayimg.mk
+++ configs/core/pislide/buildroot_ext/package/rayimg/rayimg.mk
@@ -1,3 +1,9 @@
+################################################################################
+#
+# rayimg
+#
+################################################################################
+
 RAYIMG_VERSION = 0.0.4
 RAYIMG_SITE = https://github.com/JarvyJ/rayimg
 RAYIMG_SITE_METHOD = git
@@ -6,4 +12,3 @@ RAYIMG_BUILD_TARGETS += cmd/rayimg
 RAYIMG_INSTALL_BINS += rayimg
 RAYIMG_DEPENDENCIES += libgbm libdrm
 RAYIMG_TAGS += drm,noaudio,rpi
-$(eval $(golang-package))
