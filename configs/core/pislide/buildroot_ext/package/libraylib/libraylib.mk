9da675b6aa3d9fb4dfd5f25d4a1c5b712ad5802d
Jarvy Jarvison <88567611+JarvyJ@users.noreply.github.com>
2025-08-30 22:54:35 -0500

    added libraylib (untested)

---
+++ configs/core/pislide/buildroot_ext/package/libraylib/libraylib.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# libraylib
+#
+################################################################################
+
+LIBRAYLIB_VERSION = 5.5
+LIBRAYLIB_SITE = $(call github,raysan5,raylib,$(LIBRAYLIB_VERSION))
+LIBRAYLIB_INSTALL_STAGING = YES
+LIBFOO_INSTALL_TARGET = NO
+LIBRAYLIB_LICENSE = Zlib
+LIBRAYLIB_LICENSE_FILES = LICENSE
+
+LIBRAYLIB_DEPENDENCIES += libdrm libegl libgles libgbm
+LIBRAYLIB_CONF_OPTS += -DPLATFORM=PLATFORM_DRM -DBUILD_SHARED_LIBS=ON
+
+
+$(eval $(cmake-package))
