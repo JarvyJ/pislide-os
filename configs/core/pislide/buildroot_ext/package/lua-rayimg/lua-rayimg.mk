660461c84ee0335e6aa13e64835557776a8edc29
Jarvy Jarvison <88567611+JarvyJ@users.noreply.github.com>
2025-09-18 19:26:31 -0500

    cleaned these up and got them working!

--- configs/core/pislide/buildroot_ext/package/lua-rayimg/lua-rayimg.mk
+++ configs/core/pislide/buildroot_ext/package/lua-rayimg/lua-rayimg.mk
@@ -10,10 +10,10 @@ LUA_RAYIMG_SITE = https://github.com/JarvyJ/rayimg/releases/download/1.0.0
 LUA_RAYIMG_LICENSE = AGPL
 LUA_RAYIMG_SUBDIR = rayimg
 LUA_RAYIMG_LICENSE_FILES = $(LUA_RAYIMG_SUBDIR)/LICENSE
-LUA_INSTALL_TARGET_CMDS = y
+LUA_RAYIMG_INSTALL_TARGET_CMDS = y
 
-define LUA_INSTALL_TARGET_CMDS
+define LUA_RAYIMG_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/$(LUA_RAYIMG_SUBDIR)/static/rayimg $(TARGET_DIR)/usr/bin/rayimg
-endif
+endef
 
 $(eval $(luarocks-package))
